fa7c96512d514be43fdcf98e133cad311dfb6205
Satish Kumar Singh <76210028+satishkumarsingh2024@users.noreply.github.com>
2024-03-15 23:36:24 +0530

    Add files via upload

---
+++ TheTable_v1.0.py
@@ -0,0 +1,16 @@
+print(" __________________________")
+print("|                          |")
+print("|  Application: The Table  |")
+print("|  Version: 1.0            |")
+print("|__________________________|")
+
+print()
+
+Table = int(input("Table >>> "))
+
+print()
+
+for i in range(1, 11):
+    print(Table, "x", i, "=", Table*i)
+
+print()
